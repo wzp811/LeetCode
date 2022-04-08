148311e21de13bb069cddd10c2a225e5e86a04e5
wzp811 <811235852@qq.com>
2022-04-08 11:56:27 +0800

    2022-4-8 每日一题

---
+++ src/QuestOfTheDay/No429_LevelOrder.java
@@ -0,0 +1,28 @@
+package QuestOfTheDay;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class No429_LevelOrder {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> cur = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                for (Node child : node.children) {
+                    queue.add(child);
+                }
+                cur.add(node.val);
+            }
+            ans.add(cur);
+        }
+        return ans;
+    }
+}
