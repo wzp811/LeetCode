45f84fc8e47e5cec69e10a9011a1343b1fb80214
wzp811 <811235852@qq.com>
2022-04-22 11:39:24 +0800

    2022-4-22 每日一题+剑指offer专项突击五道

---
+++ src/Coding_Interviews/KthLargest.java
@@ -0,0 +1,22 @@
+package Coding_Interviews;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    PriorityQueue<Integer> queue;
+    int capacity;
+    public KthLargest(int k, int[] nums) {
+        queue = new PriorityQueue<>();
+        capacity = k;
+        for (int i = 0; i < nums.length; i++) {
+            queue.add(nums[i]);
+        }
+        while (queue.size() > capacity) queue.poll();
+    }
+
+    public int add(int val) {
+        queue.add(val);
+        if (queue.size() > capacity) queue.poll();
+        return queue.peek();
+    }
+}
---
+++ src/Coding_Interviews/No57_ContainsNearbyAlmostDuplicate.java
@@ -0,0 +1,21 @@
+package Coding_Interviews;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class No57_ContainsNearbyAlmostDuplicate {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Long> set = new TreeSet<>();
+        int i = 0;
+        while (i < nums.length) {
+            if (i >= k) set.remove((long)nums[i - k]);
+            Long prev = set.floor((long)nums[i]);
+            Long next = set.ceiling((long)nums[i]);
+            if (prev != null && Math.abs((long)prev - nums[i]) <= t || next != null && Math.abs((long)next - nums[i]) <= t) return true;
+            set.add((long)nums[i]);
+            i++;
+        }
+        return false;
+    }
+}
