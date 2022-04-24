8beb3539aebed499293c2620508c550f428bfec8
wzp811 <811235852@qq.com>
2022-04-24 12:45:28 +0800

    2022-4-23 每日一题+剑指offer专项突击五道

---
+++ src/Coding_Interviews/MagicDictionary.java
@@ -0,0 +1,47 @@
+package Coding_Interviews;
+
+public class MagicDictionary {
+    class Node {
+        boolean end;
+        Node[] child;
+        Node() {
+            end = false;
+            child = new Node[26];
+            for (int i = 0; i < 26; i++) child[i] = null;
+        }
+    }
+    Node root;
+    /** Initialize your data structure here. */
+    public MagicDictionary() {
+        root = new Node();
+    }
+
+    public void buildDict(String[] dictionary) {
+        for (String word: dictionary) {
+            Node cur = root;
+            for (int i = 0; i < word.length(); i++) {
+                int index = word.charAt(i) - 'a';
+                if (cur.child[index] == null) cur.child[index] = new Node();
+                cur = cur.child[index];
+            }
+            cur.end = true;
+        }
+    }
+
+    public boolean search(String searchWord) {
+        return process(searchWord, 0, false, root);
+    }
+    public boolean process(String searchWord, int index, boolean hasChange, Node cur) {
+        if (index == searchWord.length()) return cur != null && cur.end && hasChange;
+        if (cur == null) return false;
+        int digit = searchWord.charAt(index) - 'a';
+        if (hasChange) {
+            return process(searchWord, index + 1, true, cur.child[digit]);
+        } else {
+            for (int i = 0; i < 26; i++) {
+                if (process(searchWord, index + 1, i != digit, cur.child[i])) return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/Coding_Interviews/No65_MinimumLengthEncoding.java
@@ -0,0 +1,25 @@
+package Coding_Interviews;
+
+import java.util.HashSet;
+
+public class No65_MinimumLengthEncoding {
+    public int minimumLengthEncoding(String[] words) {
+        HashSet<String> set = new HashSet<>();
+        for (String word : words) {
+            set.add(word);
+        }
+        for (String word : words) {
+            for (int i = 1; i < word.length(); i++) {
+                if (set.contains(word.substring(i))) {
+                    set.remove(word.substring(i));
+                }
+            }
+        }
+        int length = 0;
+        for (String word : set) {
+            length += word.length();
+        }
+        length += set.size() - 1;
+        return length;
+    }
+}
