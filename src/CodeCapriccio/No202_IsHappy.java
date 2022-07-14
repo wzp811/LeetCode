c96486db4086b16b4f3e5834245c5175604341cf
wzp811 <811235852@qq.com>
2022-07-14 16:57:22 +0800

    HashMap

---
+++ src/CodeCapriccio/No202_IsHappy.java
@@ -0,0 +1,19 @@
+package CodeCapriccio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class No202_IsHappy {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (!set.contains(n) && n != 1) {
+            int newNum = 0;
+            while (n > 0) {
+                newNum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = newNum;
+        }
+        return n == 1;
+    }
+}
