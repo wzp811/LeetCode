96b120bd69c589693041f55feb20eeb4977239f2
wzp811 <811235852@qq.com>
2022-07-25 01:42:35 +0800

    2022-7-25

---
+++ src/Y2022_July/CBTInserter.java
@@ -0,0 +1,39 @@
+package Y2022_July;
+
+import Coding_Interviews.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CBTInserter {
+
+    TreeNode root;
+
+    public CBTInserter(TreeNode root) {
+        this.root = root;
+    }
+
+    public int insert(int val) {
+        TreeNode node = new TreeNode(val);
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.pollFirst();
+            if (cur.left == null) {
+                cur.left = node;
+                return cur.val;
+            } else if (cur.right == null) {
+                cur.right = node;
+                return cur.val;
+            } else {
+                queue.addLast(cur.left);
+                queue.addLast(cur.right);
+            }
+        }
+        return 0;
+    }
+
+    public TreeNode get_root() {
+        return root;
+    }
+}
