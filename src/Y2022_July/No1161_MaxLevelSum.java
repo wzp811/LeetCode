631dc9a1fa0e8c185b2826e610ce838fda2652da
wzp811 <811235852@qq.com>
2022-08-01 09:53:00 +0800

    2022-8-1

---
+++ src/Y2022_July/No1161_MaxLevelSum.java
@@ -0,0 +1,32 @@
+package Y2022_July;
+
+import Hot100.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class No1161_MaxLevelSum {
+    public int maxLevelSum(TreeNode root) {
+        int max = root.val;
+        int maxLevel = 1;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.pollFirst();
+                sum += cur.val;
+                if (cur.left != null) queue.addLast(cur.left);
+                if (cur.right != null) queue.addLast(cur.right);
+            }
+            if (sum > max) {
+                max = sum;
+                maxLevel = level;
+            }
+        }
+        return maxLevel;
+    }
+}
