c8733d3c953e20290c2ffda0c8efdcfa03a6c803
wzp811 <811235852@qq.com>
2022-08-04 09:10:46 +0800

    2022-8-4

---
+++ src/CodeCapriccio/No70_ClimbStairs.java
@@ -0,0 +1,13 @@
+package CodeCapriccio;
+
+public class No70_ClimbStairs {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
