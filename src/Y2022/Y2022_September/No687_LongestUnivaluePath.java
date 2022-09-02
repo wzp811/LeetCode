99ca056ab2f2f587b03b3831976be6faa72ea932
wzp811 <811235852@qq.com>
2022-09-02 13:15:39 +0800

    2022-9-2

---
+++ src/Y2022/Y2022_September/No687_LongestUnivaluePath.java
@@ -0,0 +1,25 @@
+package Y2022.Y2022_September;
+
+import Hot100.TreeNode;
+
+public class No687_LongestUnivaluePath {
+    int res = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    public int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        int left1 = 0, right1 = 0;
+        if (root.left != null && root.left.val == root.val) {
+            left1 = left + 1;
+        }
+        if (root.right != null && root.right.val == root.val) {
+            right1 = right + 1;
+        }
+        res = Math.max(res, left1 + right1);
+        return Math.max(left1, right1);
+    }
+}
